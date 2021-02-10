d2e48ee04644f0f88f83938dba89685b5a913475
JoseChee <josechee.unp@gmail.com>
2021-02-10 17:15:03 -0600

    Creando el repositorio y en viando los primeros archivos de prueba grupo y grupo(2)

---
+++ grupo (2).m
@@ -0,0 +1,14 @@
+clear all;
+#g = imread('C:/Users/Public/finalPseniales/1.jpg');
+g = imread('C:/Users/Public/finalPseniales/2.jpg');
+imshow(g)
+
+
+#save(figura,'train\282.jpg');  
+
+
+
+
+
+
+
---
+++ grupo.m
@@ -0,0 +1,16 @@
+clear all;
+#g = imread('282.jpg');
+#IMAGE_PATH = ('C:\Users\Public\finalPseniales'); 
+IMAGE_PATH = ('C:\Users\...'); 
+Image = imread(IMAGE_PATH, '2.jpg');
+#g = imread('1.jpg');
+imshow(Image)
+
+#save(figura,'train\282.jpg');  
+
+
+
+
+
+
+
