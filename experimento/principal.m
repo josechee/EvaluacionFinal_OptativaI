a20c6b6a5f43577516cd82c8f520de0acd613d5d
JoseChee <josechee.unp@gmail.com>
2021-02-12 18:50:16 -0600

    Se actualizo el reporte y aun se esta ejecutando el meta clasificador

--- experimento/principal.m
+++ experimento/principal.m
@@ -2,7 +2,6 @@ clear all;
 TRAIN=load('BD_TRAIN');
 
 # programacion Evolutiva
-
 [CLASES,BD_SIN_CLASES]=quitar_obtener_Clases(TRAIN); %% quitar las clases y obtener solo las clases
                                                      %% sirve tambien obtener solo la base de datos sin la clase
 senial_F1= BD_SIN_CLASES(1,:); #obtener la primera senial o la primera fila de la BD sin la clase
@@ -11,8 +10,8 @@ w1_min = 2;
 w1_max = length(senial_F1-2000); 
 
 w2= ventanas(senial_F1);## obtener el porcentaje de ventanas que se mandara de la la primera senia (fila 1)
-w2_min = 1;%round(w2(1)) # aplicar el redondeo hacia arriba si es >=.5
-w2_max = 1;%round(w2(2)) # aplicar el redondeo hacia abajo si es <.5
+w2_min = round(w2(1)); # aplicar el redondeo hacia arriba si es >=.5
+w2_max = round(w2(2)); # aplicar el redondeo hacia abajo si es <.5
 
 k_min = 1;
 k_max = 1;%%%aqui era 100 
@@ -26,8 +25,8 @@ Lim_min = [w1_min,w2_min,k_min];  # limite inferior % se va tener un vector de d
 
 #Lim_max = [261,212,19];   # limite superior % se va tener un vector de diferente limite pejp = [LongitudSenial,LongitudSenial,100] 
 #Lim_min = [146,170,5];
-P = 2;       # tamanio de la poblacion
-G = 2;        # numero de generaciones
+P = 5;       # tamanio de la poblacion
+G = 10;        # numero de generaciones
 PM = 0.9;     # probabilidad de mutacion
 
 #####################
@@ -45,25 +44,19 @@ evaluaciones = fCalcularAptitud(poblacion,P,TRAIN);
 
 #------------------------Inicia las iteraciones-----------------------------
 
-for i=1:1##Aqui es G
+for i=1:G##Aqui es G
   #Operador de mutacion  
   hijos = fOperadorMutacion(poblacion,Lim_min,Lim_max,P,D,PM);
-  eval_hijos = fCalcularAptitud_HIJOS(hijos,P,TRAIN);##era solo fCalcularAptitud
-  
+  eval_hijos = fCalcularAptitud_HIJOS(hijos,P,TRAIN);
   #Torneos estocasticos
   #juntar las poblaciones tanto de los padres como de los hijos
   poblacionTotal = [poblacion;hijos];
   evaluacionTotal = [evaluaciones,eval_hijos];
-  #seleccionPoblacion_hijos = torneo(poblacion,hijos,2);
-  
-  seleccionEvaluaciones_eval_hijos1 = torneo(poblacionTotal,evaluacionTotal,2);% al reves esto si funciona
-  #seleccionPoblacion_eval_hijos1 = torneo(poblacion,eval_hijos,2);
-  
+  #seleccionPoblacion_hijos = torneo(poblacion,hijos,2);  
+  seleccionEvaluaciones_eval_hijos1 = torneo(poblacionTotal,evaluacionTotal,2);
+  #seleccionPoblacion_eval_hijos1 = torneo(poblacion,eval_hijos,2);  
   #seleccionEvaluaciones_eval_hijos2 = torneo(eval_hijos,evaluaciones,2);
-  #seleccion_PobTotal_evaluacionTotal = torneo(poblacion,hijos,2);
-  #Checar por acá en esta parte decidir o ver masomenos de que forma se va evaluar este en la seleccion por torneo
-  #si con poblacionTotal con evaluacionTotal o con los subconjuntos de este    
-  #####  
+  #seleccion_PobTotal_evaluacionTotal = torneo(poblacion,hijos,2); 
 endfor
 
 ##RESULT
--- experimento/ventanas.m
+++ experimento/ventanas.m
@@ -2,7 +2,7 @@ function [ventana] = ventanas(senial)
   
   ventana=[];
   longSenial=length(senial);
-  ventana =(longSenial*5)/100;%tamanio de ventana o limite inferior #20%
-  ventana =[ventana,(longSenial*50)/100];%tamanio de ventana o limite superior#90%
+  ventana =(longSenial*2)/100;%tamanio de ventana o limite inferior #20%
+  ventana =[ventana,(longSenial*20)/100];%tamanio de ventana o limite superior#90%
 
 endfunction
